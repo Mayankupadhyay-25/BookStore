2bafb4098abba932459fd8eb7162c62e8f8c1a74
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-10-01 23:44:13 +0530

    adjustment

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import Logo from "../../assets/website/logo.png"
+import Logo from "../../assets/website/logo.png";
+import { FaSortDown } from "react-icons/fa";
 
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Seller",
+    link: "/#services",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="container">
+    <div className="shadow-lg">
+      <div className="container py-3 sm:y-0">
         <div className="flex justify-between items-center">
           <div>
-            <a href="#">
-              <img src={Logo} alt="" />
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} alt="" className="w-10" />
+              Books
             </a>
-            Book</div>
+          </div>
           <div>
-            <ul>
-              <li>Home</li>
+            <ul className=" items-center gap-4 hidden sm:flex">
+              {Menu.map((menu) => (
+                <li>
+                  <a href={menu.link} className="inline-block py-4 px-4 hover:text-blue-00 duration-200 ">
+                    {menu.name}</a>
+                </li>
+              ))}
+              {/* dropdown section */}
+              <li className="group relative cursor-pointer">
+                <a href="/#" className="flex h-[72px] items-center gap-[2px]">Quick links</a>
+                <span>
+                  <FaSortDown 
+                  className="transiction duration-300 group-hover:rotate-180"/>
+                </span>
+              </li>
             </ul>
-            
           </div>
         </div>
       </div>
