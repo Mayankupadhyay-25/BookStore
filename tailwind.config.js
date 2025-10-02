2d539df51e24bf7dcf5730745d670a5ab5b41153
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-10-02 21:40:16 +0530

    error

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,15 @@ export default {
                 secondary:"#2aa6df",
                 dark:"#1e1e1e",
                 light: "#f5f5f5"
-                }
+                },
             container: {
                 center: true,
                 padding:{
                     DEFAULT: "1rem",
                     sm: "2rem",
                    
-                }
-            }
+                },
+            },
         },
     },
     plugins:[],
